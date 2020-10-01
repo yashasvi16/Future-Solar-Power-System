651ea14db6ec316f1ee55aac724071b0a700db59
satyam-ninetails <72228470+satyam-ninetails@users.noreply.github.com>
2020-10-02 02:22:37 +0530

    Update FutureSOPOCode.ino

--- FutureSOPOCode.ino
+++ FutureSOPOCode.ino
@@ -10,18 +10,18 @@ void setup() {
    pinMode(A1, INPUT);//inputs for the sensor   
   pinMode(A0, INPUT);
      servo.attach(12);
-Serial.begin(9600);
-servo.write(90);
+Serial.begin(9640);
+servo.write(96);
 }
 
 void loop() {
   //Coding for LDR sensor
 light1=analogRead(A1);//Inputs in LDR sensors
 light2=analogRead(A0);
-Serial.println(light1);
 Serial.println(light2);
+Serial.println(light1);
 
-//We are using LDR sensors to detect light sensitivity, of sunlight for the same
+//We are using LDR sensors to detect light sensitivity, of sunlight for the same work
   //program for LDR sensors
 int diff1=abs(light1-light2);
 int diff2=abs(light2-light1);
@@ -37,6 +37,6 @@ if((diff1 <= error) || (diff2 <= error)) {
       ip=ip+1; 
     }
   }
- servo.write(ip); //Tells servo to go in position
-  delay(100);//Delay of 100ms, to reach it's position
+ servo.write(ip); //Tells servo to go in position quickly
+  delay(100);//Delay of 100ms, to reach it's position quickly
  }
